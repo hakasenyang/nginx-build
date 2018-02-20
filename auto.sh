738c35cd2abbc47b80a5d926423a05d51270a61f
Hakase <hakase@hakase.io>
2018-02-20 14:35:40 +0900

    Add submodule - nginx-module-vts

--- auto.sh
+++ auto.sh
@@ -87,11 +87,12 @@ auto/configure \
 --add-module=./lib/ngx_pagespeed ${PS_NGX_EXTRA_FLAGS} \
 --add-module=./lib/ngx-fancyindex \
 --add-module=./lib/nginx-ct \
+--add-module=./lib/naxsi/naxsi_src \
+--add-module=./lib/nginx-module-vts \
 --add-module=./lib/nginx-dav-ext-module \
 --add-module=./lib/_s/headers-more-nginx-module \
 --add-module=./lib/_s/ngx_cache_purge \
---add-module=./lib/_s/set-misc-nginx-module \
---add-module=./lib/naxsi/naxsi_src
+--add-module=./lib/_s/set-misc-nginx-module
 
 
 ### OpenSSL Skip
