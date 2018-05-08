0e8309c17dd26e29c3c9e6681aacee7c5f6ccc21
Hakase <hakase@hakase.io>
2018-05-08 23:49:42 +0900

    Remodified shell script

--- auto.sh
+++ auto.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-### ld config
-### -lrt -ljemalloc -Wl,-z,relro -Wl,-z,now -fPIC
+
+### Please install library.
+### jemalloc jemalloc-devel libuuid ... the other...
 
 ### Remove Old file
 rm -f /usr/sbin/nginx.old
@@ -44,7 +45,7 @@ fi
 
 auto/configure \
 --with-cc-opt='-DTCP_FASTOPEN=23 -m64 -flto -g -O3 -march=native -fstack-protector-strong -fuse-ld=gold -fuse-linker-plugin --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wno-strict-aliasing -Wp,-D_FORTIFY_SOURCE=2 -gsplit-dwarf -DNGX_HTTP_HEADERS' \
---with-ld-opt='-ljemalloc -Wl,-z,relro' \
+--with-ld-opt='-lrt -ljemalloc -Wl,-z,relro -Wl,-z,now -fPIC' \
 --with-openssl-opt="enable-tls13downgrade enable-ec_nistp_64_gcc_128 enable-weak-ssl-ciphers no-ssl3-method -DCFLAGS='-O3 -march=native -fuse-linker-plugin -ljemalloc'" \
 --builddir=objs --prefix=/usr/local/nginx \
 --conf-path=/etc/nginx/nginx.conf \
