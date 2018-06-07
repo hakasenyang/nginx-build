1d8d1778a9483dce00d8076d59e9178b8885e91e
Hakase <hakase@hakase.app>
2018-06-07 15:36:25 +0900

    Fix spdy server header.

--- src/http/ngx_http_spdy_filter_module.c
+++ src/http/ngx_http_spdy_filter_module.c
@@ -177,8 +177,12 @@ ngx_http_spdy_header_filter(ngx_http_request_t *r)
 
     if (r->headers_out.server == NULL) {
         len += ngx_http_spdy_nv_nsize("server");
-        len += clcf->server_tokens ? ngx_http_spdy_nv_vsize(NGINX_VER)
-                                   : ngx_http_spdy_nv_vsize("nginx");
+        if (clcf->server_tokens == NGX_HTTP_SERVER_TOKENS_ON)
+            len += ngx_http_spdy_nv_vsize(NGINX_SERVER_FULL);
+        else if (clcf->server_tokens == NGX_HTTP_SERVER_TOKENS_BUILD)
+            len += ngx_http_spdy_nv_vsize(NGINX_SERVER_FULL_BUILD);
+        else
+            len += ngx_http_spdy_nv_vsize(NGINX_SERVER);
     }
 
     if (r->headers_out.date == NULL) {
@@ -328,9 +332,13 @@ ngx_http_spdy_header_filter(ngx_http_request_t *r)
 
     if (r->headers_out.server == NULL) {
         last = ngx_http_spdy_nv_write_name(last, "server");
-        last = clcf->server_tokens
-               ? ngx_http_spdy_nv_write_val(last, NGINX_VER)
-               : ngx_http_spdy_nv_write_val(last, "nginx");
+
+        if (clcf->server_tokens == NGX_HTTP_SERVER_TOKENS_ON)
+            last = ngx_http_spdy_nv_write_val(last, NGINX_SERVER_FULL);
+        else if (clcf->server_tokens == NGX_HTTP_SERVER_TOKENS_BUILD)
+            last = ngx_http_spdy_nv_write_val(last, NGINX_SERVER_FULL_BUILD);
+        else
+            last = ngx_http_spdy_nv_write_val(last, NGINX_SERVER);
 
         count++;
     }
