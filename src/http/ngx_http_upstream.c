33cd571e4102d60b3ae1df5fe06ac0207c091d13
Hakase <hakase@hakase.app>
2018-06-07 09:54:00 +0900

    Rollback SPDY module and SSL config (TLSv1, TLSv1.1)

--- src/http/ngx_http_upstream.c
+++ src/http/ngx_http_upstream.c
@@ -520,6 +520,12 @@ ngx_http_upstream_init(ngx_http_request_t *r)
         return;
     }
 #endif
+#if (NGX_HTTP_SPDY)
+    if (r->spdy_stream) {
+        ngx_http_upstream_init_request(r);
+        return;
+    }
+#endif
 
     if (c->read->timer_set) {
         ngx_del_timer(c->read);
@@ -1338,6 +1344,11 @@ ngx_http_upstream_check_broken_connection(ngx_http_request_t *r,
 
 #if (NGX_HTTP_V2)
     if (r->stream) {
+        return;
+    }
+#endif
+#if (NGX_HTTP_SPDY)
+    if (r->spdy_stream) {
         return;
     }
 #endif
