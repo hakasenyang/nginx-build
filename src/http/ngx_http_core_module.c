c7794990e8ad47457b6c1d8213d1563f27ee6d38
Hakase <hakase@hakase.app>
2018-10-08 20:31:42 +0900

    Fixed for http configuration only - strict_sni, strict_sni_header

--- src/http/ngx_http_core_module.c
+++ src/http/ngx_http_core_module.c
@@ -442,14 +442,14 @@ static ngx_command_t  ngx_http_core_commands[] = {
       NULL },
 
     { ngx_string("strict_sni"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+      NGX_HTTP_MAIN_CONF|NGX_CONF_FLAG,
       ngx_conf_set_flag_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
       offsetof(ngx_http_core_loc_conf_t, strict_sni),
       NULL },
 
     { ngx_string("strict_sni_header"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+      NGX_HTTP_MAIN_CONF|NGX_CONF_FLAG,
       ngx_conf_set_flag_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
       offsetof(ngx_http_core_loc_conf_t, strict_sni_header),
